f6bbd780eeffc01fa5a048c102f0af234f5b363d
CHANDU <CHANDU@192.168.0.112>
2019-11-27 19:50:27 +0530

    3Cmt

---
+++ testNG/src/test/java/com/testngannotations/maven/testNG/TestNGEx1.java
@@ -0,0 +1,21 @@
+package com.testngannotations.maven.testNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestNGEx1 {
+	@Test
+	public void m1() throws InterruptedException {
+		System.out.println("Hellow World..");
+		WebDriverManager.chromedriver().setup();
+		WebDriver webdrv= new ChromeDriver();
+		webdrv.navigate().to("https://mail.google.com/");
+		
+		Thread.sleep(4000);
+		webdrv.quit();
+		
+	}
+}
