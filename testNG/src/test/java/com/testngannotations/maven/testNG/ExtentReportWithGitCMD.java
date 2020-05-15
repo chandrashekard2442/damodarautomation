67535cf315fe4cee01c3f3a3d2a1bc8ee72639ad
Chandrasheakr Damodara <dchandrashekar2442@gmail.com>
2020-05-15 18:05:37 +0530

    for testing purpose comment line added

--- testNG/src/test/java/com/testngannotations/maven/testNG/ExtentReportWithGitCMD.java
+++ testNG/src/test/java/com/testngannotations/maven/testNG/ExtentReportWithGitCMD.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ExtentReportWithGitCMD {
-	
+	//for the basics of extent reports
 	@Test
 	public void createdInstance() throws InterruptedException {
 		System.out.println("Hellow World..");
